cfc3126b5813018f0c03595857fca430352aa50d
dev-arekusandoru <66217747+dev-arekusandoru@users.noreply.github.com>
2023-08-24 16:35:22 -0400

    setup template

--- Prayer-Circle/babel.config.js
+++ Prayer-Circle/babel.config.js
@@ -1,7 +1,10 @@
 module.exports = function (api) {
-  plugins: ["nativewind/babel"],
-    api.cache(true);
+  api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      // Required for expo-router
+      'expo-router/babel',
+    ],
   };
 };
