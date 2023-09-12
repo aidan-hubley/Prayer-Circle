1eb4f41b6703b15b2b6282def66f20f139b2881a
Aidanhubley <ah1632@messiah.edu>
2023-09-12 03:31:44 -0400

    learning components + react + color changes :)

--- app/index.js
+++ app/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { SafeAreaView, Text, View, TextInput, Image, StatusBar } from "react-native";
+import { SafeAreaView, Text, View, TextInput, Image, StatusBar, Pressable } from "react-native";
 import { styled } from "nativewind";
 import { Button } from "../components/Button";
 
@@ -9,54 +9,56 @@ const StyledSafeArea = styled(SafeAreaView);
 const StyledView = styled(View);
 const StyledText = styled(Text);
 const StyledInput = styled(TextInput);
+const StyledPressable = styled(Pressable);
 
 export default function Login() {
 	return (
 		<SafeAreaView style={{ flex: 1, backgroundColor: "#5946B2" }}>
-			<StyledStatusBar backgroundColor="#5946B2" />
-			<StyledView className="flex flex-col pt-10 pb-5 px-[15px] items-center justify-start w-screen gap-y-1">
+			<StatusBar backgroundColor="#5946B2" />
+			<StyledView className="flex flex-col pt-4 pb-5 px-[15px] items-center justify-start w-screen gap-y-1">
 				<StyledView className="w-full flex flex-col items-center justify-center">
-					<StyledText className="text-offwhite text-center text-5xl font-bold">
+					<StyledText className="text-[#F7F1E3] text-center text-5xl font-bold">
 						Welcome to Prayer Circle!!
 					</StyledText>
-					<StyledImage
-						className="w-100"
-						source={require('../assets/PCLogo.png')}
-						resizeMode="contain"
-					/>
+					<StyledView className="w-70% my-2">
+						<StyledImage
+							source={require('../assets/PCLogo.png')}
+							resizeMode="contain"
+						/>
+					</StyledView>
 				</StyledView>
 				<StyledView className="flex flex-col items-center justify-center w-full gap-y-5">
 					<StyledInput
-						className="bg-offblack text-[18px] w-11/12 text-offwhite border border-offwhite rounded-lg px-3 py-[10px]"
+						className="bg-offblack text-[18px] w-11/12 text-[#F7F1E3] border border-[#F7F1E3] rounded-lg px-3 py-[10px]"
 						placeholder={"Name"}
-						placeholderTextColor={"#fff"}
+						placeholderTextColor={"#F7F1E3"}
 					/>
 					<StyledInput
-						className="bg-offblack text-[18px] w-11/12 text-offwhite border border-offwhite rounded-lg px-3 py-[10px]"
+						className="bg-offblack text-[18px] w-11/12 text-[#F7F1E3] border border-[#F7F1E3] rounded-lg px-3 py-[10px]"
 						placeholder={"Email"}
-						placeholderTextColor={"#fff"}
+						placeholderTextColor={"#F7F1E3"}
 					/>
 					<StyledInput
-						className="bg-offblack text-[18px] w-11/12 text-offwhite border border-offwhite rounded-lg px-3 py-[10px]"
+						className="bg-offblack text-[18px] w-11/12 text-[#F7F1E3] border border-[#F7F1E3] rounded-lg px-3 py-[10px]"
 						placeholder={"Phone Number"}
-						placeholderTextColor={"#fff"}
+						placeholderTextColor={"#F7F1E3"}
 					/>
 					<StyledInput
-						className="bg-offblack text-[18px] w-11/12 text-offwhite border border-offwhite rounded-lg px-3 py-[10px]"
+						className="bg-offblack text-[18px] w-11/12 text-[#F7F1E3] border border-[#F7F1E3] rounded-lg px-3 py-[10px]"
 						placeholder={"Password"}
-						placeholderTextColor={"#fff"}
+						placeholderTextColor={"#F7F1E3"}
 					/>
 				</StyledView>
 			</StyledView>
 			<StyledView className="flex flex-col items-center">
-				<Button width="w-[85%]" title="Register" />
-				<StyledText className="text-offwhite text-center text-[18px] mt-5">
+				<Button width="w-[85%]" title="Register" textColor="#F7F1E3" backgroundColor="#121212" borderColor="#F9A826" />
+				<StyledText className="text-[#F7F1E3] text-center text-[18px] mt-5">
 					Already have an account?{" "}
 					<StyledText className="text-[#F9A826] font-bold">
 						Login
 					</StyledText>
 				</StyledText>
 			</StyledView>
-		</SafeAreaView>
+		</SafeAreaView >
 	);
 }
--- components/Button.js
+++ components/Button.js
@@ -5,18 +5,22 @@ import { styled } from "nativewind";
 const StyledText = styled(Text);
 const StyledPressable = styled(Pressable);
 
-export function Button({ title, onPress, width, textSize, textStyles }) {
+export function Button({ title, onPress, width, textSize, textStyles, backgroundColor, textColor, borderColor }) {
+	const bgColor = backgroundColor ? backgroundColor : "#F7F1E3";
+	const txtColor = textColor ? textColor : "#121212";
+	const borderClr = borderColor ? borderColor : "#F7F1E3";
+
 	return (
 		<StyledPressable
-			className={`flex bg-white h-[50px] items-center justify-center rounded-full ${
-				width ? width : "w-11/12"
-			}`}
+			className={`flex h-[50px] items-center justify-center rounded-full bg-[${bgColor}] border border-[${borderClr}]
+				${width ? width : "w-11/12"}
+			`}
+			style={{ borderWidth: 1, borderColor: borderClr }}
 			onPress={onPress}
 		>
 			<StyledText
-				className={`text-black font-bold ${
-					textSize ? textSize : "text-[20px]"
-				} ${textStyles}`}
+				className={`font-bold text-[${txtColor}] ${textSize ? textSize : "text-[20px]"
+					} ${textStyles}`}
 			>
 				{title}
 			</StyledText>
