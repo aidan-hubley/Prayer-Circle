e113284a2668101a0e2ae61dd7eab4731b55cb9a
dev-arekusandoru <66217747+dev-arekusandoru@users.noreply.github.com>
2023-09-19 17:12:52 -0400

    routing button v1

---
+++ components/RoutingButton.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import { Text, Pressable } from "react-native";
+import { styled } from "nativewind";
+
+const StyledText = styled(Text);
+const StyledPressable = styled(Pressable);
+
+export function Button({
+	title,
+	width,
+	textSize,
+	textStyles,
+	backgroundColor,
+	textColor,
+	borderColor,
+	href
+}) {
+	let bgColor = backgroundColor ? backgroundColor : "#F7F1E3";
+	let txtColor = textColor ? textColor : "#121212";
+	let borderClr = borderColor ? borderColor : "#F7F1E3";
+
+	return (
+		<Link href={href}>
+			<StyledPressable
+				className={`flex h-[50px] items-center justify-center rounded-full bg-offwhite border border-[${borderClr}]
+				${width ? width : "w-11/12"}
+			`}
+			>
+				<StyledText
+					className={`font-bold text-[${txtColor}] ${
+						textSize ? textSize : "text-[20px]"
+					} ${textStyles}`}
+				>
+					{title}
+				</StyledText>
+			</StyledPressable>
+		</Link>
+	);
+}
