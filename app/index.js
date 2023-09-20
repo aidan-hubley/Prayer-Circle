647ef66e0137f63f836b22871f0aabef68a3ea8e
dev-arekusandoru <66217747+dev-arekusandoru@users.noreply.github.com>
2023-09-19 20:14:33 -0400

    register inputs respect keyboard

--- app/index.js
+++ app/index.js
@@ -4,21 +4,15 @@ import {
 	Text,
 	View,
 	TextInput,
-	StatusBar,
 	Keyboard,
-	ScrollView,
-	StyleSheet,
-	Image
+	Image,
+	TouchableWithoutFeedback,
+	StatusBar
 } from "react-native";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 import { styled } from "nativewind";
 import { Button } from "../components/Button";
-import {
-	loginUser,
-	readData,
-	writeData,
-	createCircle
-} from "../database/firebaseFunctions";
-import { Link } from "expo-router";
+import { writeData, registerUser } from "../database/firebaseFunctions";
 
 const StyledImage = styled(Image);
 const StyledSafeArea = styled(SafeAreaView);
@@ -27,7 +21,11 @@ const StyledText = styled(Text);
 const StyledInput = styled(TextInput);
 
 export default function Login() {
+	const [fname, setFName] = useState("");
+	const [lname, setLName] = useState("");
+	const [username, setUsername] = useState("");
 	const [email, setEmail] = useState("");
+	/* const [phone, setPhone] = useState(""); */
 	const [pass, setPass] = useState("");
 
 	return (
@@ -36,44 +34,160 @@ export default function Login() {
 				className="bg-offblack"
 				style={{ flex: 1 /*  backgroundColor: "#5946B2"  */ }}
 			>
-				<ScrollView keyboardDismissMode="on-drag">
-					<StyledView className="flex flex-col pt-10 pb-5 px-[15px] items-center justify-start w-screen ">
-						<StyledView className="w-full flex flex-col items-center justify-center mb-2">
-							<StyledText className="text-offwhite text-center text-5xl font-bold">
-								Login to Prayer Circle!
-							</StyledText>
-							<StyledView className="w-[40%] aspect-square my-2">
-								<StyledImage
-									className="w-full h-full"
-									source={require("../assets/PCLogo.png")}
-									resizeMode="contain"
-								/>
+				<KeyboardAwareScrollView>
+					<TouchableWithoutFeedback
+						onPress={Keyboard.dismiss}
+						keyboardDismissMode="on-drag"
+					>
+						<>
+							<StyledView className="flex flex-col pt-10 pb-5 px-[15px] items-center justify-start w-screen ">
+								<StyledView className="w-full flex flex-col items-center justify-center mb-2">
+									<StyledText className="text-offwhite text-center text-5xl font-bold">
+										Welcome to Prayer Circle!!
+									</StyledText>
+									<StyledView className="w-[40%] aspect-square my-2">
+										<StyledImage
+											className="w-full h-full"
+											source={require("../assets/PCLogo.png")}
+											resizeMode="contain"
+										/>
+									</StyledView>
+								</StyledView>
+								<StyledView className="flex flex-col items-center justify-center w-full gap-y-4">
+									<StyledInput
+										className=" bg-offblack text-[18px] w-11/12 text-offwhite border border-offwhite rounded-lg px-3 py-[10px]"
+										placeholder={"Username"}
+										placeholderTextColor={"#fff"}
+										inputMode="text"
+										maxLength={30}
+										ref={(input) => {
+											this.usernameInput = input;
+										}}
+										onSubmitEditing={() => {
+											this.fNameInput.focus();
+										}}
+										blurOnSubmit={false}
+										onEndEditing={(text) => {
+											setUsername(text.nativeEvent.text);
+										}}
+									/>
+									<StyledView className="flex flex-row w-11/12">
+										<StyledInput
+											className="bg-offblack text-[18px] mr-1 w-auto flex-1 text-offwhite border border-offwhite rounded-lg px-3 py-[10px]"
+											placeholder={"First Name"}
+											placeholderTextColor={"#fff"}
+											inputMode="text"
+											maxLength={30}
+											ref={(input) => {
+												this.fNameInput = input;
+											}}
+											onSubmitEditing={() => {
+												this.lNameInput.focus();
+											}}
+											autoComplete="given-name"
+											blurOnSubmit={false}
+											onEndEditing={(text) => {
+												setFName(text.nativeEvent.text);
+											}}
+										/>
+										<StyledInput
+											className="bg-offblack text-[18px] ml-1 w-auto flex-1 text-offwhite border border-offwhite rounded-lg px-3 py-[10px]"
+											placeholder={"Last Name"}
+											placeholderTextColor={"#fff"}
+											inputMode="text"
+											maxLength={30}
+											ref={(input) => {
+												this.lNameInput = input;
+											}}
+											onSubmitEditing={() => {
+												this.emailInput.focus();
+											}}
+											autoComplete="family-name"
+											blurOnSubmit={false}
+											onEndEditing={(text) => {
+												setLName(text.nativeEvent.text);
+											}}
+										/>
+									</StyledView>
+									<StyledInput
+										className="bg-offblack text-[18px] w-11/12 text-offwhite border border-offwhite rounded-lg px-3 py-[10px]"
+										placeholder={"Email"}
+										placeholderTextColor={"#fff"}
+										inputMode="email"
+										autoComplete="email"
+										maxLength={30}
+										ref={(input) => {
+											this.emailInput = input;
+										}}
+										onSubmitEditing={() => {
+											this.passInput.focus();
+										}}
+										blurOnSubmit={false}
+										onEndEditing={(text) => {
+											setEmail(text.nativeEvent.text);
+										}}
+									/>
+									<StyledInput
+										className="bg-offblack text-[18px] w-11/12 text-offwhite border border-offwhite rounded-lg px-3 py-[10px]"
+										placeholder={"Password"}
+										placeholderTextColor={"#fff"}
+										secureTextEntry={true}
+										maxLength={25}
+										ref={(input) => {
+											this.passInput = input;
+										}}
+										onEndEditing={(text) => {
+											setPass(text.nativeEvent.text);
+										}}
+									/>
+								</StyledView>
 							</StyledView>
-						</StyledView>
-					</StyledView>
-					<StyledView className="w-full flex flex-col items-center justify-center">
-						<Button
-							width="w-[85%] mb-3"
-							title="Login"
-							onPress={() => {
-								console.log("login");
-							}}
-						/>
-						<Button
-							width="w-[85%]"
-							title="Register"
-							onPress={() => {
-								console.log("register");
-							}}
-						/>
-					</StyledView>
-				</ScrollView>
+							<StyledView className="flex flex-col items-center">
+								<Button
+									width="w-[85%]"
+									title="Register"
+									press={() => {
+										Keyboard.dismiss();
+										createUserData(
+											username,
+											fname,
+											lname,
+											email,
+											pass
+										);
+									}}
+								/>
+								<StyledText className="text-offwhite text-center text-[18px] mt-5">
+									Already have an account?{" "}
+									<StyledText className="text-[#F9A826] font-bold">
+										Login
+									</StyledText>
+								</StyledText>
+							</StyledView>
+						</>
+					</TouchableWithoutFeedback>
+				</KeyboardAwareScrollView>
+				<StatusBar barStyle={"light-content"} />
 			</StyledSafeArea>
 		</>
 	);
 }
-createCircle({
-	name: "test",
-	description: "test",
-	owner: "test"
-});
+
+function createUserData(username, fname, lname, email, password) {
+	//clear all fields
+	this.usernameInput.clear();
+	this.fNameInput.clear();
+	this.lNameInput.clear();
+	this.emailInput.clear();
+	this.passInput.clear();
+
+	let userData = {
+		username: username,
+		fname: fname,
+		lname: lname,
+		email: email,
+		phone: "0000000000",
+		created: new Date().getTime()
+	};
+	registerUser(email, password, userData);
+}
