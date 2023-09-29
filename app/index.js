763b24339c5a80f93f1a1878ada66c3a8cd17174
dev-arekusandoru <66217747+dev-arekusandoru@users.noreply.github.com>
2023-09-29 16:43:30 -0400

    post type selector

--- app/index.js
+++ app/index.js
@@ -1,11 +1,11 @@
-import React from "react";
-import { SafeAreaView, Text, View, StatusBar } from "react-native";
-import { styled } from "nativewind";
-import { Button } from "../components/Buttons";
-import { signOut } from "firebase/auth";
-import { auth, router } from "../backend/config";
-import { userLoggedIn } from "../backend/firebaseFunctions";
-import AsyncStorage from "@react-native-async-storage/async-storage";
+import React from 'react';
+import { SafeAreaView, Text, View, StatusBar } from 'react-native';
+import { styled } from 'nativewind';
+import { PostTypeSelector } from '../components/PostTypeSelector';
+import { signOut } from 'firebase/auth';
+import { auth, router } from '../backend/config';
+import { userLoggedIn } from '../backend/firebaseFunctions';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const StyledSafeArea = styled(SafeAreaView);
 const StyledView = styled(View);
@@ -14,8 +14,15 @@ const StyledText = styled(Text);
 export default function Page() {
 	userLoggedIn();
 
-	AsyncStorage.getItem("user").then((user) => {
+	/* AsyncStorage.getItem("user").then((user) => {
 		if (!user || user.length == 0) router.push("/login");
 		else router.push("/feed");
-	});
+	}); */
+	return (
+		<StyledSafeArea className='bg-offblack border' style={{ flex: 1 }}>
+			<StyledView className='flex-1 items-center mt-20 pt-10'>
+				<PostTypeSelector></PostTypeSelector>
+			</StyledView>
+		</StyledSafeArea>
+	);
 }
