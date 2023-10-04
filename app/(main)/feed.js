ba510164687e018693fd696f2cab9bf3907a15e5
dev-arekusandoru <66217747+dev-arekusandoru@users.noreply.github.com>
2023-10-04 13:47:05 -0400

    fixed feed and posts to render better on more screen sizes. updated circle to not require press() eclaration

--- app/(main)/feed.js
+++ app/(main)/feed.js
@@ -24,100 +24,97 @@ export default function Page() {
 		<StyledSafeArea className='bg-offblack border' style={{ flex: 1 }}>
 			<StyledView className='flex-1 items-center' w-screen>
 				<StyledScrollView className='w-full px-[13px]'>
-					<StyledView className='w-full h-[95px]' />
-					<Post
-						user='Alex Muresan'
-						img='https://i.imgur.com/0y8Ftya.png'
-						title='Pray for my dog he is very sick'
-						timestamp={1695846631107}
-						content='He is very sick blah blah blah blah oh blah blah blah blah blah blah blah
+					<StyledView className='w-full flex justify-center items-center'>
+						<StyledView className='w-full h-[95px]' />
+						<Post
+							user='Alex Muresan'
+							img='https://i.imgur.com/0y8Ftya.png'
+							title='Pray for my dog he is very sick'
+							timestamp={1695846631107}
+							content='He is very sick blah blah blah blah oh blah blah blah blah blah blah blah
 blah blah blah blah blah blah blah oh no he’s gonna die ahhhhhhhhhhh'
-						icon='heart-outline'
-					/>
-					<Post
-						user='Alex Muresan'
-						img='https://i.imgur.com/0y8Ftya.png'
-						title='Pray for my dog he is very sick'
-						timestamp={1695846631107}
-						content='He is very sick blah blah blah blah oh blah blah blah blah blah blah blah
+							icon='heart-outline'
+						/>
+						<Post
+							user='Alex Muresan'
+							img='https://i.imgur.com/0y8Ftya.png'
+							title='Pray for my dog he is very sick'
+							timestamp={1695846631107}
+							content='He is very sick blah blah blah blah oh blah blah blah blah blah blah blah
 blah blah blah blah blah blah blah oh no he’s gonna die ahhhhhhhhhhh'
-						icon='heart-outline'
-					/>
-					<Post
-						user='Alex Muresan'
-						img='https://i.imgur.com/0y8Ftya.png'
-						title='Pray for my dog he is very sick'
-						timestamp={1695846631107}
-						content='He is very sick blah blah blah blah oh blah blah blah blah blah blah blah
+							icon='heart-outline'
+						/>
+						<Post
+							user='Alex Muresan'
+							img='https://i.imgur.com/0y8Ftya.png'
+							title='Pray for my dog he is very sick'
+							timestamp={1695846631107}
+							content='He is very sick blah blah blah blah oh blah blah blah blah blah blah blah
 blah blah blah blah blah blah blah oh no he’s gonna die ahhhhhhhhhhh'
-						icon='heart-outline'
-					/>
-					<Post
-						user='Alex Muresan'
-						img='https://i.imgur.com/0y8Ftya.png'
-						title='Pray for my dog he is very sick'
-						timestamp={1695846631107}
-						content='He is very sick blah blah blah blah oh blah blah blah blah blah blah blah
+							icon='heart-outline'
+						/>
+						<Post
+							user='Alex Muresan'
+							img='https://i.imgur.com/0y8Ftya.png'
+							title='Pray for my dog he is very sick'
+							timestamp={1695846631107}
+							content='He is very sick blah blah blah blah oh blah blah blah blah blah blah blah
 blah blah blah blah blah blah blah oh no he’s gonna die ahhhhhhhhhhh'
-						icon='heart-outline'
-					/>
-					<Post
-						user='Alex Muresan'
-						img='https://i.imgur.com/0y8Ftya.png'
-						title='Pray for my dog he is very sick'
-						timestamp={1695846631107}
-						content='He is very sick blah blah blah blah oh blah blah blah blah blah blah blah
+							icon='heart-outline'
+						/>
+						<Post
+							user='Alex Muresan'
+							img='https://i.imgur.com/0y8Ftya.png'
+							title='Pray for my dog he is very sick'
+							timestamp={1695846631107}
+							content='He is very sick blah blah blah blah oh blah blah blah blah blah blah blah
 blah blah blah blah blah blah blah oh no he’s gonna die ahhhhhhhhhhh'
-						icon='heart-outline'
-					/>
-					<Post
-						user='Alex Muresan'
-						img='https://i.imgur.com/0y8Ftya.png'
-						title='Pray for my dog he is very sick'
-						timestamp={1695846631107}
-						content='He is very sick blah blah blah blah oh blah blah blah blah blah blah blah
+							icon='heart-outline'
+						/>
+						<Post
+							user='Alex Muresan'
+							img='https://i.imgur.com/0y8Ftya.png'
+							title='Pray for my dog he is very sick'
+							timestamp={1695846631107}
+							content='He is very sick blah blah blah blah oh blah blah blah blah blah blah blah
 blah blah blah blah blah blah blah oh no he’s gonna die ahhhhhhhhhhh'
-						icon='heart-outline'
-					/>
-					<Post
-						user='Alex Muresan'
-						img='https://i.imgur.com/0y8Ftya.png'
-						title='Pray for my dog he is very sick'
-						timestamp={1695846631107}
-						content='He is very sick blah blah blah blah oh blah blah blah blah blah blah blah
+							icon='heart-outline'
+						/>
+						<Post
+							user='Alex Muresan'
+							img='https://i.imgur.com/0y8Ftya.png'
+							title='Pray for my dog he is very sick'
+							timestamp={1695846631107}
+							content='He is very sick blah blah blah blah oh blah blah blah blah blah blah blah
 blah blah blah blah blah blah blah oh no he’s gonna die ahhhhhhhhhhh'
-						icon='heart-outline'
-					/>
-					<Post
-						user='Alex Muresan'
-						img='https://i.imgur.com/0y8Ftya.png'
-						title='Pray for my dog he is very sick'
-						timestamp={1695846631107}
-						content='He is very sick blah blah blah blah oh blah blah blah blah blah blah blah
+							icon='heart-outline'
+						/>
+						<Post
+							user='Alex Muresan'
+							img='https://i.imgur.com/0y8Ftya.png'
+							title='Pray for my dog he is very sick'
+							timestamp={1695846631107}
+							content='He is very sick blah blah blah blah oh blah blah blah blah blah blah blah
 blah blah blah blah blah blah blah oh no he’s gonna die ahhhhhhhhhhh'
-						icon='heart-outline'
-					/>
-					<Post
-						user='Alex Muresan'
-						img='https://i.imgur.com/0y8Ftya.png'
-						title='Pray for my dog he is very sick'
-						timestamp={1695846631107}
-						content='He is very sick blah blah blah blah oh blah blah blah blah blah blah blah
+							icon='heart-outline'
+						/>
+						<Post
+							user='Alex Muresan'
+							img='https://i.imgur.com/0y8Ftya.png'
+							title='Pray for my dog he is very sick'
+							timestamp={1695846631107}
+							content='He is very sick blah blah blah blah oh blah blah blah blah blah blah blah
 blah blah blah blah blah blah blah oh no he’s gonna die ahhhhhhhhhhh'
-						icon='heart-outline'
-						end={true}
-					/>
-					<StyledView className='w-full h-[125px]' />
+							icon='heart-outline'
+							end={true}
+						/>
+						<StyledView className='w-full h-[125px]' />
+					</StyledView>
 				</StyledScrollView>
 			</StyledView>
 
 			<StyledView className='absolute bottom-10 flex flex-row justify-center w-screen'>
-				<Circle
-					size='w-[90px] h-[90px]'
-					press={() => {
-						console.log('pressed');
-					}}
-				/>
+				<Circle size='w-[90px] h-[90px]' />
 			</StyledView>
 		</StyledSafeArea>
 	);
