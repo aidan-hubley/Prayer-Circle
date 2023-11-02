4c3cabb09cf0c528d9f0c23eef9ccdfc4706a53d
dev-arekusandoru <66217747+dev-arekusandoru@users.noreply.github.com>
2023-11-02 18:07:56 -0400

    fixed circle

--- app/(main)/mainViewLayout.js
+++ app/(main)/mainViewLayout.js
@@ -55,7 +55,13 @@ export default function Layout() {
 				</StyledView>
 
 				<StyledView
-					style={{bottom: insets.bottom < 10 ? insets.bottom + 15 : insets.bottom, width: Dimensions.get("window").width - 200}}
+					style={{
+						bottom:
+							insets.bottom < 10
+								? insets.bottom + 15
+								: insets.bottom,
+						width: Dimensions.get('window').width - 200
+					}}
 					className='absolute flex flex-row mx-[100px] justify-center z-0'
 				>
 					<Circle />
