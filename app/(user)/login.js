da7eaf1859b461cfae4effa222762bee3aa87a66
Jdayres <justin5208@me.com>
2023-11-07 19:04:54 -0500

    It freakin works!!!!

--- app/(user)/login.js
+++ app/(user)/login.js
@@ -14,6 +14,7 @@ import {
 } from 'react-native';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 import { styled } from 'nativewind';
+import { sendPasswordResetEmail } from 'firebase/auth';
 import { Button } from '../../components/Buttons';
 import { Link } from 'expo-router';
 import { loginUser } from '../../backend/firebaseFunctions';
@@ -28,131 +29,114 @@ const StyledInput = styled(TextInput);
 const StyledKeyboardAwareScrollView = styled(KeyboardAwareScrollView);
 
 export default function Login() {
-	const [email, setEmail] = useState('');
-	const [pass, setPass] = useState('');
-	const [modalVisible, setModalVisible] = useState(false);
-	const [resetEmail, setResetEmail] = useState('');
+  const [email, setEmail] = useState('');
+  const [pass, setPass] = useState('');
+  const [isForgotPasswordModalVisible, setIsForgotPasswordModalVisible] = useState(false);
+  const [resetEmail, setResetEmail] = useState('');
 
-	const handlePasswordReset = async () => {
-		try {
-		await auth.sendPasswordResetEmail(resetEmail);
-		Alert.alert("Check your email", "A link to reset your password has been sent to your email address.", [
-			{ text: "OK", onPress: () => setModalVisible(false) }
-		]);
-		setResetEmail('');
-		} catch (error) {
-		Alert.alert("Error", error.message);
-		}
-	};
+  const handlePasswordReset = async () => {
+    try {
+      await sendPasswordResetEmail(auth, resetEmail);
+      Alert.alert("Check your email", "A link to reset your password has been sent to your email address.", [
+        { text: "OK", onPress: () => setIsForgotPasswordModalVisible(false) }
+      ]);
+      setResetEmail('');
+    } catch (error) {
+      Alert.alert("Error", error.message);
+    }
+  };
 
 	return (
-		<>
-			<StyledSafeArea className='flex-1 bg-offblack'>
-				<StyledKeyboardAwareScrollView className=''>
-					<TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-						<StyledView>
-							<StyledView className='flex items-center justify-center px-[15px] aspect-square my-6 w-full'>
-								<StyledImage
-									className='w-full h-full '
-									source={require('../../assets/Logo_Dark.png')}
-									resizeMode='contain'
-								/>
-							</StyledView>
-							<StyledView className='flex flex-col items-center'>
-								<StyledView className='flex flex-col items-center justify-center w-full gap-y-3 mb-3'>
-									<StyledInput
-										className='bg-offblack text-[18px] w-11/12 text-offwhite border border-offwhite rounded-lg px-3 py-[10px]'
-										placeholder={'Email'}
-										autoCapitalize='none'
-										placeholderTextColor={'#fff'}
-										inputMode='email'
-										autoComplete='email'
-										maxLength={30}
-										ref={(input) => {
-											this.emailInput = input;
-										}}
-										onSubmitEditing={() => {
-											this.passInput.focus();
-										}}
-										blurOnSubmit={false}
-										onChangeText={(text) => {
-											setEmail(text);
-										}}
-									/>
-									<StyledInput
-										className='bg-offblack text-[18px] w-11/12 text-offwhite border border-offwhite rounded-lg px-3 py-[10px]'
-										placeholder={'Password'}
-										placeholderTextColor={'#fff'}
-										secureTextEntry={true}
-										maxLength={25}
-										ref={(input) => {
-											this.passInput = input;
-										}}
-										onChangeText={(text) => {
-											setPass(text);
-										}}
-									/>
-								</StyledView>
-								<Button
-									width='w-[85%]'
-									title='Login'
-									textColor='text-offwhite'
-									bgColor='bg-offblack'
-									borderColor='border-yellow'
-									press={() => {
-										Keyboard.dismiss();
-										userLogin(email, pass);
-										setEmail('');
-										setPass('');
-									}}
-								/>
-								<StyledText className='text-offwhite text-center text-[18px] my-2'>
-									Don't have an account?{' '}
-									<Link href='/register'>
-										<StyledText className='text-yellow font-bold'>
-											Register
-										</StyledText>
-									</Link>
-								</StyledText>
+    <>
+      <StyledSafeArea className='flex-1 bg-offblack'>
+        <StyledKeyboardAwareScrollView>
+          <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+            <StyledView>
+              <StyledView className='flex items-center justify-center px-[15px] aspect-square my-6 w-full'>
+                <StyledImage
+                  className='w-full h-full'
+                  source={require('../../assets/Logo_Dark.png')}
+                  resizeMode='contain'
+                />
+              </StyledView>
+              <StyledView className='flex flex-col items-center'>
+                <StyledView className='flex flex-col items-center justify-center w-full gap-y-3 mb-3'>
+                  <StyledInput
+                    className='bg-offblack text-[18px] w-11/12 text-offwhite border border-offwhite rounded-lg px-3 py-[10px]'
+                    placeholder={'Email'}
+                    autoCapitalize='none'
+                    placeholderTextColor={'#fff'}
+                    keyboardType='email-address'
+                    onChangeText={(text) => setEmail(text)}
+                  />
+                  <StyledInput
+                    className='bg-offblack text-[18px] w-11/12 text-offwhite border border-offwhite rounded-lg px-3 py-[10px]'
+                    placeholder={'Password'}
+                    placeholderTextColor={'#fff'}
+                    secureTextEntry={true}
+                    onChangeText={(text) => setPass(text)}
+                  />
+                </StyledView>
+                <Button
+                  width='w-[85%]'
+                  title='Login'
+                  textColor='text-offwhite'
+                  bgColor='bg-offblack'
+                  borderColor='border-yellow'
+                  press={() => {
+                    Keyboard.dismiss();
+                    loginUser(email, pass);
+                    setEmail('');
+                    setPass('');
+                  }}
+                />
+                <StyledText className='text-offwhite text-center text-[18px] my-2'>
+                  Don't have an account?{' '}
+                  <Link href='/register'>
+                    <StyledText className='text-yellow font-bold'>
+                      Register
+                    </StyledText>
+                  </Link>
+                </StyledText>
+                <TouchableOpacity onPress={() => setIsForgotPasswordModalVisible(true)}>
+                  <StyledText className='text-yellow text-center text-[18px] mb-4'>
+                    Forgot Password?
+                  </StyledText>
+                </TouchableOpacity>
+              </StyledView>
+            </StyledView>
+          </TouchableWithoutFeedback>
+        </StyledKeyboardAwareScrollView>
+        <StatusBar barStyle={'light-content'} />
+      </StyledSafeArea>
 
-								<TouchableOpacity onPress={() => setModalVisible(true)}>
-									<StyledText className='text-yellow text-center text-[18px] mb-4'>
-										Forgot Password?
-									</StyledText>
-								</TouchableOpacity>
-
-							</StyledView>
-						</StyledView>
-					</TouchableWithoutFeedback>
-				</StyledKeyboardAwareScrollView>
-				<StatusBar barStyle={'light-content'} />
-			</StyledSafeArea>
-
-				<Modal
-			animationType="slide"
-			transparent={true}
-			visible={modalVisible}
-			onRequestClose={() => {
-			setModalVisible(!modalVisible);
-			}}
-			>
-			<View style={{ marginTop: 50, backgroundColor: 'white', padding: 20 }}>
-			<Text>Enter your email here</Text>
-			<TextInput
-				placeholder="Email"
-				value={resetEmail}
-				onChangeText={setResetEmail}
-				autoCapitalize="none"
-				keyboardType="email-address"
-			/>
-			<Button
-				title='Submit'
-				// ... add your button styling
-				press={handlePasswordReset}
-			/>
-			</View>
-		</Modal>
-		</>
+      {/* Forgot Password Modal */}
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={isForgotPasswordModalVisible}
+        onRequestClose={() => setIsForgotPasswordModalVisible(false)}
+      >
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'rgba(0, 0, 0, 0.5)' }}>
+          <View style={{ backgroundColor: 'white', padding: 20, borderRadius: 10, width: '80%', alignItems: 'center' }}>
+            <Text style={{ marginBottom: 15 }}>Enter your email here</Text>
+            <TextInput
+              style={{ height: 40, borderColor: 'gray', borderWidth: 1, marginBottom: 15, width: '100%', paddingHorizontal: 10 }}
+              placeholder="Email"
+              value={resetEmail}
+              onChangeText={setResetEmail}
+              autoCapitalize="none"
+              keyboardType="email-address"
+            />
+            <Button
+              title='Submit'
+              
+              press={handlePasswordReset}
+            />
+          </View>
+        </View>
+      </Modal>
+    </>
 	);
 }
 
