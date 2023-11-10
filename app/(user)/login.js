88c5e9c77482e07bbabe2d290935c1202c7139a4
dev-arekusandoru <66217747+dev-arekusandoru@users.noreply.github.com>
2023-11-09 19:58:42 -0500

    resolve login page conflicts

--- app/(user)/login.js
+++ app/(user)/login.js
@@ -9,18 +9,14 @@ import {
 	TouchableWithoutFeedback,
 	Image,
 	TouchableOpacity,
-	Alert,
-	KeyboardAvoidingView,
-	Platform
+	Alert
 } from 'react-native';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 import { styled } from 'nativewind';
 import { sendPasswordResetEmail } from 'firebase/auth';
 import { Button } from '../../components/Buttons';
-import { Link } from 'expo-router';
+import { router } from '../../backend/config';
 import { loginUser } from '../../backend/firebaseFunctions';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { auth } from '../../backend/config';
 import Modal from 'react-native-modal';
 
 const StyledImage = styled(Image);
@@ -28,7 +24,6 @@ const StyledSafeArea = styled(SafeAreaView);
 const StyledView = styled(View);
 const StyledText = styled(Text);
 const StyledInput = styled(TextInput);
-const StyledKeyboardAwareScrollView = styled(KeyboardAwareScrollView);
 const StyledModal = styled(Modal);
 
 export default function Login() {
@@ -56,110 +51,121 @@ export default function Login() {
 	};
 
 	return (
-		<>
-			<StyledSafeArea className='flex-1 bg-offblack'>
-				<StyledKeyboardAwareScrollView>
-					<TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-						<StyledView>
-							<StyledView className='flex items-center justify-center px-[15px] aspect-square my-6 w-full'>
-								<StyledImage
-									className='w-full h-full'
-									source={require('../../assets/Logo_Dark.png')}
-									resizeMode='contain'
-								/>
-							</StyledView>
-							<StyledView className='flex flex-col items-center'>
-								<StyledView className='flex flex-col items-center justify-center w-full gap-y-3 mb-3'>
-									<StyledInput
-										className='bg-offblack text-[18px] w-11/12 text-offwhite border border-offwhite rounded-lg px-3 py-[10px]'
-										placeholder={'Email'}
-										autoCapitalize='none'
-										placeholderTextColor={'#fff'}
-										keyboardType='email-address'
-										onChangeText={(text) => setEmail(text)}
-									/>
-									<StyledInput
-										className='bg-offblack text-[18px] w-11/12 text-offwhite border border-offwhite rounded-lg px-3 py-[10px]'
-										placeholder={'Password'}
-										placeholderTextColor={'#fff'}
-										secureTextEntry={true}
-										onChangeText={(text) => setPass(text)}
-									/>
-								</StyledView>
-								<Button
-									width='w-[85%]'
-									title='Login'
-									textColor='text-offwhite'
-									bgColor='bg-offblack'
-									borderColor='border-yellow'
-									press={() => {
-										Keyboard.dismiss();
-										loginUser(email, pass);
-										setEmail('');
-										setPass('');
+		<StyledSafeArea className='flex-1 bg-offblack'>
+			<KeyboardAwareScrollView bounces={false}>
+				<TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+					<StyledView className='w-screen px-[15px] flex items-center justify-center'>
+						<StyledView className='flex items-center justify-center aspect-square my-6 w-11/12'>
+							<StyledImage
+								className='w-full h-full '
+								source={require('../../assets/Squared_Logo_Dark.png')}
+								resizeMode='contain'
+							/>
+						</StyledView>
+						<StyledView className='w-full flex flex-col items-center'>
+							<StyledView className='flex flex-col items-center justify-center w-full gap-y-3 mb-3'>
+								<StyledInput
+									className='bg-offblack text-[18px] w-11/12 text-offwhite border border-outline rounded-lg px-3 py-[10px]'
+									placeholder={'Email'}
+									autoCapitalize='none'
+									placeholderTextColor={'#fff'}
+									inputMode='email'
+									autoComplete='email'
+									maxLength={30}
+									ref={(input) => {
+										this.emailInput = input;
+									}}
+									onSubmitEditing={() => {
+										this.passInput.focus();
+									}}
+									blurOnSubmit={false}
+									onChangeText={(text) => {
+										setEmail(text);
 									}}
 								/>
-								<StyledText className='text-offwhite text-center text-[18px] my-2'>
-									Don't have an account?{' '}
-									<Link href='/register'>
-										<StyledText className='text-green font-bold'>
-											Register
-										</StyledText>
-									</Link>
-								</StyledText>
-								<TouchableOpacity onPress={toggleModal1}>
-									<StyledText className='text-yellow text-center text-[18px] mb-4'>
-										Forgot Password?
-									</StyledText>
-								</TouchableOpacity>
-							</StyledView>
-						</StyledView>
-					</TouchableWithoutFeedback>
-				</StyledKeyboardAwareScrollView>
-				<StatusBar barStyle={'light-content'} />
-				<StyledModal
-					className='w-[80%] self-center h-[90%]'
-					isVisible={isModalVisible1}
-					onBackdropPress={toggleModal1}
-					avoidKeyboard={true}
-				>
-					<StyledView className='bg-offblack border-[5px] border-offwhite rounded-2xl h-[40%]'>
-						<StyledView className='flex-1 items-center h-[60%]'>
-							<StyledText className='absolute top-[6%] text-3xl text-offwhite'>
-								Reset Password
-							</StyledText>
-							<StyledText className='absolute top-[20%] text-xl text-offwhite'>
-								Enter your email here:
-							</StyledText>
-							<StyledView className='absolute top-[35%] w-[85%]'>
 								<StyledInput
-									className='text-[18px] text-offwhite border border-offwhite rounded-lg px-3 py-[10px]'
-									placeholder='Email'
-									value={resetEmail}
-									onChangeText={setResetEmail}
-									autoCapitalize='none'
-									keyboardType='email-address'
+									className='bg-offblack text-[18px] w-11/12 text-offwhite border border-outline rounded-lg px-3 py-[10px]'
+									placeholder={'Password'}
+									placeholderTextColor={'#fff'}
+									secureTextEntry={true}
+									maxLength={25}
+									ref={(input) => {
+										this.passInput = input;
+									}}
+									onChangeText={(text) => {
+										setPass(text);
+									}}
 								/>
 							</StyledView>
 							<Button
-								title='Submit'
-								btnStyles={'absolute bottom-[10%]'}
-								width='w-[70%]'
-								press={handlePasswordReset}
+								title='Login'
+								press={() => {
+									Keyboard.dismiss();
+									userLogin(email, pass);
+									setEmail('');
+									setPass('');
+								}}
 							/>
+							<StyledText className='text-offwhite text-center text-[18px] my-2'>
+								Don't have an account?{' '}
+								<TouchableWithoutFeedback
+									onPress={() => {
+										router.replace('/register');
+									}}
+								>
+									<StyledText className='text-yellow font-bold'>
+										Register
+									</StyledText>
+								</TouchableWithoutFeedback>
+							</StyledText>
+							<TouchableOpacity onPress={toggleModal1}>
+								<StyledText className='text-yellow text-center text-[18px] mb-4'>
+									Forgot Password?
+								</StyledText>
+							</TouchableOpacity>
 						</StyledView>
 					</StyledView>
-				</StyledModal>
-			</StyledSafeArea>
-		</>
+				</TouchableWithoutFeedback>
+			</KeyboardAwareScrollView>
+			<StatusBar barStyle={'light-content'} />
+			<StyledModal
+				className='w-[80%] self-center h-[90%]'
+				isVisible={isModalVisible1}
+				onBackdropPress={toggleModal1}
+				avoidKeyboard={true}
+			>
+				<StyledView className='bg-offblack border-[5px] border-offwhite rounded-2xl h-[40%]'>
+					<StyledView className='flex-1 items-center h-[60%]'>
+						<StyledText className='absolute top-[6%] text-3xl text-offwhite'>
+							Reset Password
+						</StyledText>
+						<StyledText className='absolute top-[20%] text-xl text-offwhite'>
+							Enter your email here:
+						</StyledText>
+						<StyledView className='absolute top-[35%] w-[85%]'>
+							<StyledInput
+								className='text-[18px] text-offwhite border border-offwhite rounded-lg px-3 py-[10px]'
+								placeholder='Email'
+								value={resetEmail}
+								onChangeText={setResetEmail}
+								autoCapitalize='none'
+								keyboardType='email-address'
+							/>
+						</StyledView>
+						<Button
+							title='Submit'
+							btnStyles={'absolute bottom-[10%]'}
+							width='w-[70%]'
+							press={handlePasswordReset}
+						/>
+					</StyledView>
+				</StyledView>
+			</StyledModal>
+		</StyledSafeArea>
 	);
 }
 
 function userLogin(email, password) {
-	//clear all fields
-	this.emailInput.clear();
-	this.passInput.clear();
-
 	if (email.length == 0 || password.length == 0)
 		return alert('Please fill out all fields');
 
