d96e8b52607f459071f11737f03ce4928f62ca38
Statmosphere <andrewrroberti01@gmail.com>
2023-11-10 18:24:52 -0500

    Should be ready to merge now.
    I made all of the requested changes.

--- backend/firebaseFunctions.js
+++ backend/firebaseFunctions.js
@@ -69,7 +69,7 @@ export async function createCircle(data) {
 	data.members[`${UID}`] = true;
 	data.admin[`${UID}`] = true;
 	data.owner = UID;
-	data.codes = { admin: admin_Code, public: public_Code };
+	data.codes = { adminCode: 0, publicCode: 0 };
 
 	let circles = Object.keys((await readData(`prayer_circle/circles`)) || {});
 	let allCodes = [];
@@ -77,29 +77,24 @@ export async function createCircle(data) {
 		let circle = circles[i];
 		let circleData =
 			(await readData(`prayer_circle/circles/${circle}/codes`)) || {};
-		/*let circleStruct = {
-			adminCode: circleData.adminCode,
-			publicCode: circleData.publicCode
-		};*/
-		allCodes.push(circleData.admin_Code);
-		allCodes.push(circleData.public_Code);
+		allCodes.push(circleData.adminCode);
+		allCodes.push(circleData.publicCode);
 	}
-	data.admin_Code = 0;
-	data.public_Code = 0;
-	while (data.admin_Code === 0 || data.public_Code === 0) {
-		if (data.admin_Code === 0) {
+
+	while (data.codes.adminCode === 0 || data.codes.publicCode === 0) {
+		if (data.codes.adminCode === 0) {
 			let tempAdminCode = Math.floor(Math.random() * 90000000 + 10000000);
 			if (!allCodes.includes(tempAdminCode)) {
-				data.admin_Code = tempAdminCode;
+				data.codes.adminCode = tempAdminCode;
 				allCodes.push(tempAdminCode);
 			}
 		}
-		if (data.public_Code === 0) {
+		if (data.codes.publicCode === 0) {
 			let tempPublicCode = Math.floor(
 				Math.random() * 90000000 + 10000000
 			);
 			if (!allCodes.includes(tempPublicCode)) {
-				data.public_Code = tempPublicCode;
+				data.codes.publicCode = tempPublicCode;
 			}
 		}
 	}
