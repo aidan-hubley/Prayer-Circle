14e82675f35747c6ae2cc6af05f5f4703e055393
Jdayres <justin5208@me.com>
2024-01-22 19:30:17 -0500

    Update settings.js

--- app/(settings)/settings.js
+++ app/(settings)/settings.js
@@ -9,7 +9,8 @@ import {
 } from 'react-native';
 import {
     BottomSheetModal,
-    BottomSheetBackdrop
+    BottomSheetBackdrop,
+    BottomSheetModalProvider
 } from '@gorhom/bottom-sheet';
 import { styled } from 'nativewind';
 import { signOut } from 'firebase/auth';
@@ -72,6 +73,17 @@ export default function Page() {
         );
     };
 
+    	const handle = () => {
+		return (
+			<StyledView className='flex items-center justify-center w-screen bg-grey rounded-t-[10px] pt-3'>
+				<StyledView className='w-[30px] h-[4px] rounded-full bg-[#dddddd11] mb-3' />
+				<StyledText className='text-white font-[600] text-[24px]'>
+					Change Password
+				</StyledText>
+			</StyledView>
+		);
+	};
+
     React.useEffect(() => {
         Animated.timing(togglePosition, {
             toValue: isEnabled ? 45 : 5,
@@ -81,93 +93,117 @@ export default function Page() {
     }, [isEnabled]);
 
     return (
-        <StyledSafeArea className='bg-offblack border' style={{ flex: 1 }}>
-            <StyledView className='flex-1 items-center mt-45 pt-10'>
-                <StyledText className='text-3xl text-offwhite text-center tracking-widest leading-10'>
-                    Settings
-                </StyledText>
-                {/* ... rest of the UI elements ... */}
-                <Button
-                    title='Change Password'
-                    btnStyles='border-2 border-yellow mt-5'
-                    bgColor='offblack'
-                    textStyles='text-yellow'
-                    width='w-[70%]'
-                    press={handlePresentModalPress}
-                />
-                <Button
-                    title='Sign Out'
-                    width='w-[70%]'
-                    btnStyles='mt-5'
-                    press={() => {
-                        signOut(auth);
-                        AsyncStorage.removeItem('user');
-                        AsyncStorage.removeItem('name');
-                        // Navigate to login screen or reset navigation state
-                    }}
-                />
-            </StyledView>
-
-            <BottomSheetModal
-                ref={bottomSheetModalRef}
-                index={0}
-                snapPoints={snapPoints}
-                onChange={handleSheetChanges}
-                backdropComponent={backdrop}
-                enableDismissOnClose={true}
-            >
-                <StyledView className='flex-1 bg-grey p-4'>
+         <BottomSheetModalProvider>
+            <StyledSafeArea className='bg-offblack border' style={{ flex: 1 }}>
+                <StyledView className='flex-1 items-center mt-45 pt-10'>
                     <StyledText className='text-3xl text-offwhite text-center tracking-widest leading-10'>
-                        Change Password
+                        Settings
                     </StyledText>
-                    <StyledInput
-                                className='mt-5 p-2 w-[80%] border-[1px] border-outline rounded-xl'
-                                placeholder="Current Password"
-                                secureTextEntry={true}
-                                value={currentPassword}
-                                onChangeText={setCurrentPassword}
-                            />
-                            <StyledInput
-                                className='mt-5 p-2 w-[80%] border-[1px] border-outline rounded-xl'
-                                placeholder="New Password"
-                                secureTextEntry={true}
-                                value={newPassword}
-                                onChangeText={setNewPassword}
-                            />
-                            <StyledInput
-                                className='mt-5 p-2 w-[80%] border-[1px] border-outline rounded-xl'
-                                placeholder="Confirm New Password"
-                                secureTextEntry={true}
-                                value={confirmPassword}
-                                onChangeText={setConfirmPassword}
-                            />
-
-
-                            <Button
-                                title='Confirm'
-                                btnStyles='mt-5 border-2 border-yellow'
-                                bgColor='bg-offblack'
-                                textStyles='text-yellow'
-                                width='w-[70%]'
-                                press={() => bottomSheetModalRef.current?.dismiss()}
-                            />
-
-                            <Button
-                                title='Change With Email'
-                                btnStyles='mt-5 border-2 border-yellow'
-                                bgColor='bg-offblack'
-                                textStyles='text-yellow'
-                                width='w-[70%]'
-                                press={handlePasswordReset}
-                            />
-                            <Button
-                                title='Cancel'
-                                btnStyles='mt-5'
-                                width='w-[70%]'
-                                press={() => bottomSheetModalRef.current?.dismiss()}
-                            />
+                        <View className="flex-row items-center mt-5 px-5">
+                            <View className="flex-row justify-between items-center bg-grey p-3 w-full rounded-xl">
+                                <Text className="mr-3 text-lg text-offwhite">
+                                    Notifications
+                                </Text>
+                            <TouchableOpacity onPress={toggleSwitch}>
+                                <StyledView
+                                    className='pt-9 w-[80px] h-[30px] rounded-full'
+                                    style={{
+                                        backgroundColor: isEnabled
+                                            ? '#00A55E'
+                                            : '#F9A826'
+                                    }}
+                                >
+                                    <StyledAnimatedView
+                                        className='absolute top-1 w-[28px] h-[28px] rounded-full bg-white	'
+                                        style={{
+                                            left: togglePosition
+                                        }}
+                                    />
+                                </StyledView>
+                            </TouchableOpacity>
+                            </View>
+                        </View>
+                    <Button
+                        title='Change Password'
+                        btnStyles='border-2 border-yellow mt-5'
+                        bgColor='offblack'
+                        textStyles='text-yellow'
+                        width='w-[70%]'
+                        press={handlePresentModalPress}
+                    />
+                    <Button
+                        title='Sign Out'
+                        width='w-[70%]'
+                        btnStyles='mt-5'
+                        press={() => {
+                            signOut(auth);
+                            AsyncStorage.removeItem('user');
+                            AsyncStorage.removeItem('name');
+                            // Navigate to login screen or reset navigation state
+                        }}
+                    />
                 </StyledView>
-            </BottomSheetModal>
-        </StyledSafeArea>
+
+                <BottomSheetModal
+                    enableDismissOnClose={true}
+                    ref={bottomSheetModalRef}
+                    index={0}
+                    snapPoints={snapPoints}
+                    onChange={handleSheetChanges}
+                    handleComponent={handle}
+                    backdropComponent={(backdropProps) => backdrop(backdropProps)}
+                    keyboardBehavior='extend'
+                >
+                    <StyledView className='flex-1 bg-grey p-4 items-center'>
+                        <StyledInput
+                                    className='mt-5 p-2 w-[80%] border-[1px] border-outline rounded-xl'
+                                    placeholder="Current Password"
+                                    secureTextEntry={true}
+                                    value={currentPassword}
+                                    onChangeText={setCurrentPassword}
+                                />
+                                <StyledInput
+                                    className='mt-5 p-2 w-[80%] border-[1px] border-outline rounded-xl'
+                                    placeholder="New Password"
+                                    secureTextEntry={true}
+                                    value={newPassword}
+                                    onChangeText={setNewPassword}
+                                />
+                                <StyledInput
+                                    className='mt-5 p-2 w-[80%] border-[1px] border-outline rounded-xl'
+                                    placeholder="Confirm New Password"
+                                    secureTextEntry={true}
+                                    value={confirmPassword}
+                                    onChangeText={setConfirmPassword}
+                                />
+
+
+                                <Button
+                                    title='Confirm'
+                                    btnStyles='mt-5 border-2 border-yellow'
+                                    bgColor='bg-offblack'
+                                    textStyles='text-yellow'
+                                    width='w-[70%]'
+                                    press={() => bottomSheetModalRef.current?.dismiss()}
+                                />
+
+                                <Button
+                                    title='Change With Email'
+                                    btnStyles='mt-5 border-2 border-yellow'
+                                    bgColor='bg-offblack'
+                                    textStyles='text-yellow'
+                                    width='w-[70%]'
+                                    press={handlePasswordReset}
+                                />
+                                <Button
+                                    title='Cancel'
+                                    btnStyles='mt-5'
+                                    width='w-[70%]'
+                                    press={() => bottomSheetModalRef.current?.dismiss()}
+                                />
+                    </StyledView>
+                </BottomSheetModal>
+            </StyledSafeArea>
+        </BottomSheetModalProvider>
     );
 }
