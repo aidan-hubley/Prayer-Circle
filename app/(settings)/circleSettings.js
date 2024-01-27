2bd84d1db096e055b64b947f16879d3e8961e697
Statmosphere <andrewrroberti01@gmail.com>
2024-01-27 10:31:16 -0500

    Finishing merging

--- app/(settings)/circleSettings.js
+++ app/(settings)/circleSettings.js
@@ -1,9 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useMemo, useCallback } from 'react';
 import {
 	Text,
 	View,
 	Platform,
-	Pressable,
 	Animated,
 	ScrollView,
 	FlatList
@@ -14,12 +13,16 @@ import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { Button } from '../../components/Buttons';
 import { Member } from '../../components/Member.js';
 import { LinearGradient } from 'expo-linear-gradient';
+import {
+	BottomSheetModal,
+	BottomSheetFlatList,
+	BottomSheetBackdrop
+} from '@gorhom/bottom-sheet';
 
 const StyledView = styled(View);
 const StyledText = styled(Text);
 const StyledAnimatedView = styled(Animated.createAnimatedComponent(View));
 const StyledScrollView = styled(ScrollView);
-const StyledPressable = styled(Pressable);
 const StyledModal = styled(Modal);
 const StyledGradient = styled(LinearGradient);
 
@@ -36,17 +39,16 @@ export default function Page() {
 		setModalVisible2(!isModalVisible2);
 	};
 
+	// bottom sheet modal
+	const snapPoints = useMemo(() => ['85%'], []);
+	const handlePresentModalPress = useCallback(() => {
+		bottomSheetModalRef.current?.present();
+	}, []);
+	const handleSheetChanges = useCallback((index) => {}, []);
+
 	const [isEnabled, setIsEnabled] = useState(false);
 	const toggleSwitch = () => setIsEnabled((previousState) => !previousState);
 	const togglePosition = React.useRef(new Animated.Value(1)).current;
-
-	const filtermembers = () => {
-		const roleOrder = ['own', 'mod', 'mem', 'sus', 'ban'];
-		const sortedData = dummyData.sort((a, b) => {
-			return roleOrder.indexOf(a.role) - roleOrder.indexOf(b.role);
-		});
-		setDummyData([...sortedData]);
-	};
 
 	const Trevor =
 		'https://media.licdn.com/dms/image/C4E03AQEjKbD7qFuQJQ/profile-displayphoto-shrink_200_200/0/1574282480254?e=1701907200&v=beta&t=1BizKLULm5emiKX3xlsRq7twzFTqynOsfTlbRwqNuXI';
@@ -59,182 +61,67 @@ export default function Page() {
 		}).start();
 	}, [isEnabled]);
 
-	const originalOrder = [
+	const dummyData = [
 		{
 			key: '1',
 			name: 'Josh Philips',
 			username: 'JoshuaP.149134',
-			role: 'owner',
+			role: 'own',
 			img: Trevor
 		},
 		{
 			key: '2',
 			name: 'Alex Muresan',
 			username: 'muresanCoder.20',
-			role: 'admin',
+			role: 'mod',
 			img: Trevor
 		},
 		{
 			key: '3',
 			name: 'Nason Allen',
 			username: 'AllenNasin0987654',
-			role: 'admin',
+			role: 'mod',
 			img: Trevor
 		},
 		{
 			key: '4',
 			name: 'Aidan Hubley',
 			username: 'HubleyPraying',
-			role: 'admin',
+			role: 'ban',
 			img: Trevor
 		},
 		{
 			key: '5',
-			name: 'Trevor Bunch long name',
+			name: 'Trevor Bunch',
 			username: 'BunchTrevoraccount',
-			role: 'member',
+			role: 'mem',
 			img: Trevor
 		},
 		{
 			key: '6',
 			name: 'Another Account',
 			username: 'ExampleAccount1',
-			role: 'restricted',
+			role: 'sus',
 			img: Trevor
 		},
 		{
 			key: '7',
 			name: 'Another Account',
 			username: 'ExampleAccount2',
-			role: 'member',
+			role: 'mem',
 			img: Trevor
 		},
 		{
 			key: '8',
 			name: 'Another Account',
 			username: 'ExampleAccount3',
-			role: 'member',
-			img: Trevor
-		},
-		{
-			key: '9',
-			name: 'Another Account',
-			username: 'ExampleAccount4',
-			role: 'admin',
-			img: Trevor
-		},
-		{
-			key: '10',
-			name: 'Another Account',
-			username: 'ExampleAccount5',
-			role: 'member',
-			img: Trevor
-		},
-		{
-			key: '11',
-			name: 'Another Account',
-			username: 'ExampleAccount6',
-			role: 'restricted',
-			img: Trevor
-		},
-		{
-			key: '12',
-			name: 'Another Account',
-			username: 'ExampleAccount7',
-			role: 'banned',
-			img: Trevor
-		},
-		{
-			key: '13',
-			name: 'Nason Allen',
-			username: 'AllenNasin0987654',
-			role: 'admin',
-			img: Trevor
-		},
-		{
-			key: '14',
-			name: 'Aidan Hubley',
-			username: 'HubleyPraying',
-			role: 'banned',
-			img: Trevor
-		},
-		{
-			key: '15',
-			name: 'Trevor Bunch',
-			username: 'BunchTrevoraccount',
-			role: 'member',
-			img: Trevor
-		},
-		{
-			key: '16',
-			name: 'Another Account',
-			username: 'ExampleAccount1',
-			role: 'restricted',
-			img: Trevor
-		},
-		{
-			key: '17',
-			name: 'Another Account',
-			username: 'ExampleAccount2',
-			role: 'member',
-			img: Trevor
-		},
-		{
-			key: '18',
-			name: 'Another Account',
-			username: 'ExampleAccount3',
-			role: 'member',
-			img: Trevor
-		},
-		{
-			key: '19',
-			name: 'Another Account',
-			username: 'ExampleAccount4',
-			role: 'admin',
-			img: Trevor
-		},
-		{
-			key: '20',
-			name: 'Another Account',
-			username: 'ExampleAccount5',
-			role: 'member',
-			img: Trevor
-		},
-		{
-			key: '21',
-			name: 'Another Account',
-			username: 'ExampleAccount6',
-			role: 'restricted',
-			img: Trevor
-		},
-		{
-			key: '22',
-			name: 'Another Account',
-			username: 'ExampleAccount7',
-			role: 'banned',
+			role: 'mem',
 			img: Trevor
 		}
 	];
 
-	const [dummyData, setDummyData] = useState([...originalOrder]);
-	const [isSorted, setIsSorted] = useState(false);
-
-	const toggleSortOrder = () => {
-		const roleOrder = ['owner', 'admin', 'member', 'restricted', 'banned'];
-		const sortedData = isSorted
-			? [...originalOrder]
-			: [...dummyData].sort(
-					(a, b) =>
-						roleOrder.indexOf(a.role) - roleOrder.indexOf(b.role)
-			  );
-
-		setDummyData(sortedData);
-		setIsSorted((prev) => !prev); // Toggle the sorting order
-	};
-
 	return (
 		<StyledView
-			className='bg-offblack flex-1'
 			className='bg-offblack flex-1'
 			style={{ paddingTop: Platform.OS == 'android' ? insets.top : 0 }}
 		>
@@ -247,15 +134,12 @@ export default function Page() {
 					<>
 						<StyledView
 							className='w-full flex items-center mb-[10px]'
-							className='w-full flex items-center mb-[10px]'
 							style={{
 								height: 80
 							}}
 						/>
 						<StyledView className='w-full flex items-center justify-center'>
-						<StyledView className='w-full flex items-center justify-center'>
 							<Button
-								btnStyles='bg-offblack border-[8px] border-purple'
 								btnStyles='bg-offblack border-[8px] border-purple'
 								height={'h-[120px]'}
 								width={'w-[120px]'}
@@ -278,29 +162,10 @@ export default function Page() {
 								description by clicking into the box and typing.
 							</StyledText>
 						</StyledView>
-						<StyledView className='border-x border-t border-[#6666660d] mt-2 h-[50px] py-2 px-[10px] bg-grey rounded-t-[20px] flex-row items-center justify-between'>
-							{/* TODO: functioning refresh button */}
-							<Button
-								bgColor='bg-transparent'
-								height={'h-[30px]'}
-								width={'w-[30px]'}
-								iconSize={30}
-								icon='reload'
-								iconColor='#FFFBFC'
-							/>
-							<StyledText className='text-center text-[28px] text-white font-[600]'>
+						<StyledView className='border-x border-t border-[#6666660d] mt-2 w-full h-[45px] pt-2 bg-grey rounded-t-[20px] items-center justify-center'>
+							<StyledText className='w-full text-center text-[28px] text-white font-[600]'>
 								Members
 							</StyledText>
-							{/* TODO: Create deeper filtering system, search for specific roles? */}
-							<Button
-								bgColor='bg-transparent'
-								height={'h-[30px]'}
-								width={'w-[30px]'}
-								iconSize={30}
-								icon={isSorted ? 'shuffle' : 'filter-outline'}
-								iconColor='#FFFBFC'
-								press={() => toggleSortOrder()}
-							/>
 						</StyledView>
 					</>
 				}
@@ -320,7 +185,6 @@ export default function Page() {
 					<>
 						<StyledView
 							className='w-full flex items-center mb-[10px]'
-							className='w-full flex items-center mb-[10px]'
 							style={{
 								height: insets.bottom + 55
 							}}
@@ -330,23 +194,19 @@ export default function Page() {
 			/>
 			<StyledGradient
 				pointerEvents='none'
-				pointerEvents='none'
 				start={{ x: 0, y: 0.1 }}
 				end={{ x: 0, y: 1 }}
 				style={{ height: 120 }}
-				className='absolute w-screen'
 				className='absolute w-screen'
 				colors={['#121212ee', 'transparent']}
 			/>
 			<StyledView
 				style={{ top: Platform.OS == 'android' ? insets.top + 15 : 15 }}
 				className='absolute w-screen flex flex-row items-center justify-between px-[15px]'
-				className='absolute w-screen flex flex-row items-center justify-between px-[15px]'
 			>
 				<Button
 					btnStyles='rotate-180 border-2'
 					bgColor='bg-offblack'
-					borderColor='border-yellow'
 					borderColor='border-yellow'
 					height={'h-[50px]'}
 					width={'w-[50px]'}
@@ -446,21 +306,14 @@ export default function Page() {
 				<StyledView className='bg-offblack border-[5px] border-red rounded-2xl h-[60%]'>
 					<StyledView className='flex-1 items-center h-[60%]'>
 						<StyledText className='top-[6%] text-3xl text-offwhite'>
-				<StyledView className='bg-offblack border-[5px] border-red rounded-2xl h-[60%]'>
-					<StyledView className='flex-1 items-center h-[60%]'>
-						<StyledText className='top-[6%] text-3xl text-offwhite'>
 							Delete this circle?
 						</StyledText>
 
 						<Button
 							btnStyles='top-[15%] bg-grey border-4 border-purple'
-							btnStyles='top-[15%] bg-grey border-4 border-purple'
 							height={'h-[90px]'}
 							width={'w-[90px]'}
 							iconSize={60}
-							icon='musical-notes'
-							iconColor='white'
-							href='/mainViewLayout'
 							icon='musical-notes'
 							iconColor='white'
 							href='/mainViewLayout'
@@ -481,7 +334,6 @@ export default function Page() {
 						<Button
 							title='Cancel'
 							btnStyles={'top-[37%]'}
-							width='w-[70%]'
 							width='w-[70%]'
 							press={toggleModal2}
 						/>
