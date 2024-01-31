49a539f72693974a149db2f18d399d1d024dcbab
Aidanhubley <ah1632@messiah.edu>
2024-01-30 20:50:25 -0500

    Remove background color from profile image

--- app/(main)/profile.js
+++ app/(main)/profile.js
@@ -136,7 +136,6 @@ export default function ProfilePage() {
 										width: '100%',
 										height: '100%',
 										borderRadius: 18,
-										backgroundColor: 'red',
 										display: profileImage ? 'flex' : 'none'
 									}}
 									source={{
