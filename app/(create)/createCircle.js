01a1f923a515dd04563a8c12902be696b886d450
Alexandru <a.muresan8533@gmail.com>
2024-01-31 12:16:10 -0500

    add remaining components and 'create' pages

---
+++ app/(create)/createCircle.js
@@ -0,0 +1,173 @@
+import React, { useState, useRef } from 'react';
+import {
+	Text,
+	View,
+	TextInput,
+	TouchableOpacity,
+	Keyboard
+} from 'react-native';
+import { styled } from 'nativewind';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+import { Button } from '../../components/Buttons';
+import { IconSelector } from '../../components/iconSelector';
+import {
+	useSafeAreaInsets,
+	SafeAreaView
+} from 'react-native-safe-area-context';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { createCircle } from '../../backend/firebaseFunctions';
+import { ColorPicker, fromHsv } from 'react-native-color-picker';
+import Slider from '@react-native-community/slider';
+import { router } from '../../backend/config';
+import { useStore } from '../global';
+
+const StyledSafeArea = styled(SafeAreaView);
+const StyledView = styled(View);
+const StyledText = styled(Text);
+const StyledInput = styled(TextInput);
+const StyledOpacity = styled(TouchableOpacity);
+const StyledIcon = styled(Ionicons);
+const StyledColorPicker = styled(ColorPicker);
+
+export default function Page() {
+	const [title, setTitle] = useState('');
+	const [description, setDescription] = useState('');
+	const [icon, setIcon] = useState(null);
+	const [iconColor, setIconColor] = useState(null);
+	const iconSelectorRef = useRef();
+	const circleColorRef = useRef();
+	const setFilterReload = useStore((state) => state.setFilterReload);
+	let insets = useSafeAreaInsets();
+
+	const handleIconSelected = (icon, iconColor) => {
+		setIcon(icon);
+		setIconColor(iconColor);
+	};
+
+	return (
+		<StyledSafeArea className='bg-offblack flex-1'>
+			<KeyboardAwareScrollView
+				bounces={false}
+				keyboardShouldPersistTaps='handled'
+			>
+				<StyledView className='bg-offblack flex-1 flex flex-col items-center'>
+					<StyledView className='flex items-center justify-center text-center w-screen'>
+						<StyledText className='text-offwhite font-bold text-4xl'>
+							Form a Circle
+						</StyledText>
+						<StyledText className='text-offwhite text-2xl'>
+							Select a Color and Icon!
+						</StyledText>
+						<StyledText className='text-offwhite text-[15px]'>
+							To select an icon, click the circle below!
+						</StyledText>
+					</StyledView>
+					<StyledView className='relative  flex w-screen aspect-square justify-center items-center mt-4 mb-2'>
+						<StyledColorPicker
+							className='w-[100%]'
+							ref={circleColorRef}
+							sliderComponent={Slider}
+							hideSliders={true}
+							style={{ flex: 1 }}
+						/>
+						<StyledOpacity
+							className='absolute h-[40%] aspect-square bg-offblack rounded-full items-center justify-center'
+							onPress={() => {
+								iconSelectorRef.current.toggleSelector(true);
+							}}
+						>
+							<StyledIcon
+								name={icon}
+								size={85}
+								color={iconColor}
+							/>
+						</StyledOpacity>
+					</StyledView>
+					<StyledView className='w-full h-auto flex items-center justify-between px-[15px]'>
+						<StyledInput
+							className='bg-offblack text-[18px] h-[42px] w-full text-offwhite border border-offwhite rounded-lg px-3 py-[5px]'
+							placeholder={'Circle Name'}
+							placeholderTextColor={'#ffffff66'}
+							inputMode='text'
+							maxLength={22}
+							onChangeText={(text) => {
+								setTitle(text);
+							}}
+							onBlur={() => {
+								Keyboard.dismiss();
+							}}
+						/>
+						<StyledInput
+							className='bg-offblack text-[18px] w-full min-h-[42px] text-offwhite border border-offwhite rounded-lg px-3 py-[10px] my-3'
+							placeholder={'Write a bit about this Circle...'}
+							placeholderTextColor={'#ffffff66'}
+							inputMode='text'
+							maxLength={300}
+							multiline
+							rows={3}
+							onChangeText={(text) => {
+								setDescription(text);
+							}}
+							returnKeyType='done'
+							blurOnSubmit={true}
+							onBlur={() => {
+								Keyboard.dismiss();
+							}}
+						/>
+					</StyledView>
+				</StyledView>
+			</KeyboardAwareScrollView>
+			<StyledView
+				className='absolute flex flex-row w-screen px-[15px] justify-between bg-offblack pb-5'
+				style={{ bottom: insets.bottom }}
+			>
+				<Button
+					title='Erase'
+					width='w-[125px]'
+					height='h-[60px]'
+					href='/'
+					bgColor={'bg-offblack'}
+					borderColor={'border-offwhite border-2'}
+					textColor={'text-offwhite'}
+				/>
+				<Button
+					title='Draw'
+					height='h-[60px]'
+					width='w-[125px]'
+					press={async () => {
+						if (title.length == 0) {
+							alert('Please enter a title');
+							return;
+						}
+
+						let borderColor = fromHsv(
+							circleColorRef.current.state.color
+						);
+
+						let data = {
+							title: title,
+							description: description,
+							iconColor: iconColor,
+							icon: icon,
+							timestamp: Date.now(),
+							type: 'individual',
+							color: borderColor,
+							members: {},
+							admin: {},
+							owner: false
+						};
+						await createCircle(data);
+						alert('Circle Successfully Created');
+						setFilterReload(true);
+
+						router.push('/');
+					}}
+				/>
+			</StyledView>
+			<IconSelector
+				onIconSelected={handleIconSelected}
+				ref={iconSelectorRef}
+			/>
+		</StyledSafeArea>
+	);
+}
---
+++ app/(create)/createPost.js
@@ -0,0 +1,163 @@
+import React, { useRef, useState } from 'react';
+import { Text, View, TextInput, StatusBar } from 'react-native';
+import { styled } from 'nativewind';
+import { PostTypeSelector } from '../../components/PostTypeSelector';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+import { Button } from '../../components/Buttons';
+import { writeData, generateId } from '../../backend/firebaseFunctions';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { router } from '../../backend/config';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { useStore } from '../global';
+
+const StyledSafeArea = styled(SafeAreaView);
+const StyledView = styled(View);
+const StyledText = styled(Text);
+const StyledInput = styled(TextInput);
+
+export default function Page() {
+	const [title, setTitle] = useState('');
+	const [body, setBody] = useState('');
+	const [time, setTime] = useState('');
+	const typeRef = useRef();
+	const [showDatePicker, setShowDatePicker] = useState(false);
+	const [uid, name] = useStore((state) => [state.uid, state.name]);
+	const [setGlobalReload, pfp] = useStore((state) => [
+		state.setGlobalReload,
+		state.pfp
+	]);
+
+	const handleSelect = (index) => {
+		if (index == 2) {
+			setShowDatePicker(true);
+		} else {
+			setShowDatePicker(false);
+		}
+	};
+
+	return (
+		<StyledSafeArea className='bg-offblack flex-1'>
+			<KeyboardAwareScrollView bounces={false}>
+				<>
+					<StyledView className='flex items-center justify-center text-center w-screen h-[100px]'>
+						<StyledText className='text-offwhite font-bold text-4xl'>
+							Sketch a Post
+						</StyledText>
+					</StyledView>
+					<StyledView className='flex flex-col w-screen items-center py-4 px-[20px]'>
+						<StyledInput
+							className='bg-offblack text-[18px] w-full text-offwhite border border-outline rounded-lg px-3 py-[10px] my-2'
+							placeholder={'Title'}
+							placeholderTextColor={'#fff'}
+							inputMode='text'
+							autoCorrect
+							maxLength={39}
+							onChangeText={(text) => {
+								setTitle(text);
+							}}
+						/>
+						{showDatePicker && (
+							<StyledInput
+								className='bg-offblack text-[18px] w-full text-offwhite border border-outline rounded-lg px-3 py-[10px] my-2'
+								placeholder={'Event'}
+								placeholderTextColor={'#fff'}
+								inputMode='text'
+								autoCorrect
+								maxLength={39}
+								onChangeText={(text) => {
+									setTitle(text);
+								}}
+							/>
+						)}
+						<StyledInput
+							className='bg-offblack text-[18px] w-full min-h-[200px] h-[300px] max-h-[50%] text-offwhite border border-outline rounded-lg px-3 py-[10px] my-2'
+							placeholder={'Write a Post'}
+							multiline
+							autoCorrect
+							autoCapitalize='sentences'
+							placeholderTextColor={'#fff'}
+							inputMode='text'
+							maxLength={500}
+							onChangeText={(text) => {
+								setBody(text);
+							}}
+						/>
+						<PostTypeSelector
+							ref={typeRef}
+							onSelect={handleSelect}
+						/>
+					</StyledView>
+				</>
+			</KeyboardAwareScrollView>
+			<StyledView className='absolute w-screen bottom-10 flex flex-row justify-between px-[15px] mt-auto'>
+				<Button
+					title='Erase'
+					width='w-[125px]'
+					height='h-[60px]'
+					href='/index'
+					bgColor={'bg-offblack'}
+					borderColor={'border-offwhite border-2'}
+					textColor={'text-offwhite'}
+				/>
+				<Button
+					title='Draw'
+					height='h-[60px]'
+					width='w-[125px]'
+					press={async () => {
+						if (title.length == 0 || body.length == 0)
+							return alert(
+								'Please enter a title and body for your post.'
+							);
+
+						let newPostId = generateId();
+						let now = Date.now();
+						let typeSelectedVal = Math.round(
+							Math.abs(typeRef.current.selected._value)
+						);
+						let typeSelected = '';
+						if (typeSelectedVal == 0) typeSelected = 'praise';
+						else if (typeSelectedVal == 1) typeSelected = 'request';
+						else if (typeSelectedVal == 2) typeSelected = 'event';
+
+						let uid = await AsyncStorage.getItem('user');
+						let name = await AsyncStorage.getItem('name');
+
+						let newPost = {
+							user: uid,
+							profile_img: pfp,
+							name: name,
+							title: title,
+							text: body,
+							type: typeSelected,
+							timestamp: now,
+							circles: {
+								'-NhXfdEbrH1yxRqiajYm': true
+							},
+							metadata: {
+								flag_count: 0
+							}
+						};
+						await writeData(
+							`prayer_circle/posts/${newPostId}`,
+							newPost,
+							true
+						);
+						await writeData(
+							`prayer_circle/circles/-NiN-27IuGR02mcGS2CS/posts/${newPostId}`,
+							now,
+							true
+						);
+						writeData(
+							`prayer_circle/users/${uid}/private/posts/${newPostId}`,
+							true,
+							true
+						).then(() => {
+							setGlobalReload(true);
+						});
+						router.push('/');
+					}}
+				/>
+			</StyledView>
+		</StyledSafeArea>
+	);
+}
